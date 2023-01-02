42e30bbfce6484246e8c2e9244a9ab0fc4c188d6
honaseung <lhs5948@gmail.com>
2023-01-02 14:19:10 +0900

    create styling files

---
+++ blog-frontend/src/components/common/Button.js
@@ -0,0 +1,11 @@
+import '../../style/components/common/button.scss';
+
+const Button = () => {
+  return (
+    <div>
+      <button className="btn-cmm">버튼</button>
+    </div>
+  );
+};
+
+export default Button;
